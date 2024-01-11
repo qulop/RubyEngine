b2696efbd6866744e4c5e6aba5c6b663ae58d263
qulop <qlopsimail@gmail.com>
2024-01-11 18:22:40 +0300

    Event.hpp, which before include all event headers, now renamed to RubyEvents.hpp

--- ruby/src/event/Event.hpp
+++ ruby/src/event/Event.hpp
@@ -1,11 +1,36 @@
 #pragma once
 
-// Events classes
-#include "EventBase.hpp"
-#include "MouseEvent.hpp"
-#include "KeyboardEvent.hpp"
-// ------
+#include <utility/Definitions.hpp>
+#include <core/Window.hpp>
 
-// Event system
-#include "EventManager.hpp"
-// --------
+
+namespace Ruby
+{
+    enum EventType
+    {
+        RB_NONE_EVENT       = 0,
+        RB_MOUSE_PRESSED    = (1 << 1),
+        RB_MOUSE_RELEASED   = (1 << 2),
+        RB_MOUSE_MOVED      = (1 << 3),
+        RB_MOUSE_SCROLLED   = (1 << 4),
+        RB_KEY_PRESSED      = (1 << 5),
+        RB_KEY_RELEASED     = (1 << 6),
+    };
+
+    
+    class RUBY_API Event
+    {
+    public:
+        EventType GetType(void) const
+        { return m_type; }
+
+    protected:
+        Event(EventType type) :
+            m_type(type) 
+        {}
+
+    private:
+        EventType m_type = RB_NONE_EVENT;
+    };
+}
+
---
+++ ruby/src/event/RubyEvents.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+// Events classes
+#include "Event.hpp"
+#include "MouseEvent.hpp"
+#include "KeyboardEvent.hpp"
+// ------
+
+// Event system
+#include "EventManager.hpp"
+// --------

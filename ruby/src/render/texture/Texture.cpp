1c954313bae3cba4cd3c476d3185c19935ca3cae
qulop <qlopsimail@gmail.com>
2024-01-19 22:58:26 +0300

    Added structure TextureParams. TextureWrap and(new) TextureFilter now containing in this structure. Added LoadByBuffer method. Code refactoring.

--- ruby/src/render/texture/Texture.cpp
+++ ruby/src/render/texture/Texture.cpp
@@ -2,26 +2,40 @@
 
 namespace Ruby
 {
-    Texture2D::Texture2D(const RubyString& path, TextureWrap wrap)
-    {
-        LoadTexture(path, wrap);
-    }
+    Texture2D::Texture2D(const RubyString& path, TextureParams params)
+    { LoadByPath(path, params); }
 
 
-    void Texture2D::LoadTexture(const RubyString& path, TextureWrap wrap)
+    void Texture2D::LoadByPath(const RubyString& path, TextureParams params)
     {
         int width, height;
         unsigned char* image = SOIL_load_image(path.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
+        if (image == nullptr)
+        {
+            RUBY_ERROR("Failed to load texture from path {}.", path);
+            return;
+        }
 
+
+        LoadByBuffer(width, height, image, params);
+
+        SOIL_free_image_data(image);
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+
+    void Texture2D::LoadByBuffer(u32 width, u32 height, unsigned char* buffer, TextureParams params)
+    {
         glGenTextures(1, &m_texture);
         glBindTexture(GL_TEXTURE_2D, m_texture);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, params.wrap);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, params.wrap);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, params.filter);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, params.filter);
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, buffer);
 
-        SOIL_free_image_data(image);
         glBindTexture(GL_TEXTURE_2D, 0);
     }
 
@@ -40,4 +54,5 @@ namespace Ruby
 
         return m_texture;
     }
+
 }
--- ruby/src/render/texture/Texture.hpp
+++ ruby/src/render/texture/Texture.hpp
@@ -1,17 +1,32 @@
 #pragma once
 
 #include <utility/Definitions.hpp>
+#include <utility/Logger.hpp>
+
 #include <SOIL/soil.h>
 #include <glad/glad.h>
 
 namespace Ruby
 {
-    enum TextureWrap
+    struct TextureParams
     {
-        CLAMP_TO_EDGE = GL_CLAMP_TO_EDGE,
-        CLAMP_TO_BORDER = GL_CLAMP_TO_BORDER,
-        REPEAT = GL_REPEAT,
-        MIRRORED_REPEAT = GL_MIRRORED_REPEAT
+        enum TextureWrap
+        {
+            CLAMP_TO_EDGE       = GL_CLAMP_TO_EDGE,
+            CLAMP_TO_BORDER     = GL_CLAMP_TO_BORDER,
+            REPEAT              = GL_REPEAT,
+            MIRRORED_REPEAT     = GL_MIRRORED_REPEAT
+        };
+
+        enum TextureFilter
+        {
+            LINEAR              = GL_LINEAR,
+            NEAREST             = GL_NEAREST   
+        };
+
+
+        TextureFilter filter = LINEAR;
+        TextureWrap wrap = CLAMP_TO_EDGE;
     };
 
 
@@ -20,15 +35,18 @@ namespace Ruby
     public:
         Texture2D(void) = default;
 
-        Texture2D(const RubyString& path, TextureWrap wrap=CLAMP_TO_EDGE);
+        Texture2D(const RubyString& path, TextureParams params={});
 
-        void LoadTexture(const RubyString& path, TextureWrap wrap=CLAMP_TO_EDGE);
+        void LoadByPath(const RubyString& path, TextureParams params={});
+
+        void LoadByBuffer(u32 width, u32 height, unsigned char* buffer, TextureParams params={});
 
         void Use(void);
 
         void StopUsing(void);
 
         GLuint GetTextureID(void) const;
+
 
     private:
         GLuint m_texture = -1;

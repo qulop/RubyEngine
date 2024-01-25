b2500e3a789dbec9e43c426f742807aa3af4f623
qulop <qlopsimail@gmail.com>
2024-01-25 23:18:57 +0300

    The contents of render/text folder are available for include

--- ruby/src/RubyEngine.hpp
+++ ruby/src/RubyEngine.hpp
@@ -2,14 +2,12 @@
 
 #if defined(_WIN32) || defined(WIN32) || defined(__WIN32__) || defined(__NT__) || defined(_WIN64)
 
-#pragma warning(push)
-#pragma warning(disable : 4996)
-
     #include "core/RubyApp.hpp"
     #include "core/Window.hpp"
     #include "core/WindowProps.hpp"
+
     #include "event/Event.hpp"
 
-#pragma warning(pop)
+    #include "render/text/Text.hpp"
 
 #endif

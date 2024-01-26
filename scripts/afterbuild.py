18dc2296d1df124fa2fddd726acec9592719dc63
qulop <qlopsimail@gmail.com>
2024-01-26 03:34:02 +0300

    In afterbuild added function, which generate config file(demo)

--- scripts/afterbuild.py
+++ scripts/afterbuild.py
@@ -1,10 +1,54 @@
+import utility
+
 import shutil
+import pathlib
 import os
 
 
-def moveShaders(targetDirectory: str, sourceDirectory: str):
+class Path(pathlib.Path):
+    def __str__(self):
+        return super().__str__().replace("\\", "\\\\")
+
+
+def moveShaders(targetDirectory: str, sourceDirectory: str) -> None:
     if not os.path.isdir(targetDirectory):
         os.mkdir(targetDirectory)
 
     for file in os.scandir(sourceDirectory):
         shutil.copyfile(file, f"{targetDirectory}\\{file.name}")
+
+
+def createConfig(targetPath: str) -> None:
+    src = "{\n\t\"appliedShaders\" : {\n\t\t\"vertex\" : ["
+
+    shadersDir = "../ruby/src/render/shaders/list/"
+    vetexList = utility.findRecursive(shadersDir, "Vertex.*", ["glsl"])
+
+    basePath = pathlib.Path("..\\ruby\src\\render\\shaders\\list")
+    for (i, path) in vetexList:
+        path = __getRelativePath(path, basePath)
+        
+        src += path + "\", "
+    else:
+        src = src[:-2] + "],\n\t\t\"fragment\" : ["
+
+    fragmentList = utility.findRecursive(shadersDir, "Fragment.glsl", ["glsl"])
+    for (i, path) in fragmentList:
+        path = __getRelativePath(path, basePath)
+        
+        src += path + "\", "
+
+    src = src[:-2] + "]\n\t}\n}"
+
+    with open(targetPath, "w") as file:
+        file.write(src)
+
+# private
+
+def __getRelativePath(path: pathlib.WindowsPath, basePath: pathlib.Path) -> str:
+    path = path.relative_to(basePath).__str__()
+    path = "shaders\\" + path
+    path.replace("\\", "\\\\")
+
+    return "\"" + path
+

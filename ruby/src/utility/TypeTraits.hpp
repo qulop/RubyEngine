9432bb9294705a2b1e73b77e6bdcfd1eb9f0a64a
qulop <qlopsimail@gmail.com>
2024-02-05 22:05:52 +0300

    New type aliases was added

--- ruby/src/utility/TypeTraits.hpp
+++ ruby/src/utility/TypeTraits.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
-#include <string>
 #include <vector>
-#include <type_traits>
+#include <string>
 #include <concepts>
+#include <type_traits>
+#include <string_view>
 #include <unordered_map>
 
 namespace Ruby
 {
     using RubyString                        = std::string;
+
+    using RubyStringView                    = std::string_view;
 
     template<typename Tx, typename Ty>
     using RubyHashMap                       = std::unordered_map<Tx, Ty>;
@@ -17,14 +20,17 @@ namespace Ruby
     using RubyVector                        = std::vector<Tx>;
     
 
-    using u8 = uint8_t;
-    using u16 = uint16_t;
-    using u32 = uint32_t;
-    using u64 = uint64_t;
+    using u8                                = uint8_t;
+    using u16                               = uint16_t;
+    using u32                               = uint32_t;
+    using u64                               = uint64_t;
 
-    using i8 = int8_t;
-    using i16 = int16_t;
-    using i32 = int32_t;
-    using i64 = int64_t;
+    using i8                                = int8_t;
+    using i16                               = int16_t;
+    using i32                               = int32_t;
+    using i64                               = int64_t;
+
+    // C-String
+    using cstr                              = const char*;
 
 }

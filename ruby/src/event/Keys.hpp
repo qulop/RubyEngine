d409fbf009923ae196eff014a86ff35486fb9b17
qulop <qlopsimail@gmail.com>
2024-02-17 00:09:55 +0300

    Commit just so that there is at least some commit(key codes were moved from enum)

--- ruby/src/event/Keys.hpp
+++ ruby/src/event/Keys.hpp
@@ -1,144 +1,141 @@
 #pragma once
 
 
-namespace Ruby
-{
-    enum Keys
-    {
-        RB_NONE_KEY = -1,
+namespace Ruby::Keys
+{  
+    constexpr int RB_NONE_KEY = -1;
 
 
-        RB_SPACE = 32,
-        RB_APOSTROPHE = 39, // <'>
-        RB_COMMA = 44,  // <,>
-        RB_MINUS = 45, // <->
-        RB_DOT = 46,  // <.>
-        RB_SLASH = 47, // </>
+    constexpr int RB_SPACE = 32;
+    constexpr int RB_APOSTROPHE = 39; // <'>
+    constexpr int RB_COMMA = 44;  // <;>
+    constexpr int RB_MINUS = 45; // <->
+    constexpr int RB_DOT = 46;  // <.>
+    constexpr int RB_SLASH = 47; // </>
 
 
-        RB_0 = 48,
-        RB_1 = 49,
-        RB_2 = 50,
-        RB_3 = 51,
-        RB_4 = 52,
-        RB_5 = 53,
-        RB_6 = 54,
-        RB_7 = 55,
-        RB_8 = 56,
-        RB_9 = 57,
+    constexpr int RB_0 = 48;
+    constexpr int RB_1 = 49;
+    constexpr int RB_2 = 50;
+    constexpr int RB_3 = 51;
+    constexpr int RB_4 = 52;
+    constexpr int RB_5 = 53;
+    constexpr int RB_6 = 54;
+    constexpr int RB_7 = 55;
+    constexpr int RB_8 = 56;
+    constexpr int RB_9 = 57;
 
 
-        RB_SEMICOLON = 59,  // <;>
-        RB_EQUAL = 61,  // <=>
+    constexpr int RB_SEMICOLON = 59;  // <;>
+    constexpr int RB_EQUAL = 61;  // <=>
 
 
-        RB_A = 65,
-        RB_B = 66,
-        RB_C = 67,
-        RB_D = 68,
-        RB_E = 69,
-        RB_F = 70,
-        RB_G = 71,
-        RB_H = 72,
-        RB_I = 73,
-        RB_J = 74,
-        RB_K = 75,
-        RB_L = 76,
-        RB_M = 77,
-        RB_N = 78,
-        RB_O = 79,
-        RB_P = 80,
-        RB_Q = 81,
-        RB_R = 82,
-        RB_S = 83,
-        RB_T = 84,
-        RB_U = 85,
-        RB_V = 86,
-        RB_W = 87,
-        RB_X = 88,
-        RB_Y = 89,
-        RB_Z = 90,
+    constexpr int  RB_A = 65;
+    constexpr int  RB_B = 66;
+    constexpr int  RB_C = 67;
+    constexpr int  RB_D = 68;
+    constexpr int  RB_E = 69;
+    constexpr int  RB_F = 70;
+    constexpr int  RB_G = 71;
+    constexpr int  RB_H = 72;
+    constexpr int  RB_I = 73;
+    constexpr int  RB_J = 74;
+    constexpr int  RB_K = 75;
+    constexpr int  RB_L = 76;
+    constexpr int  RB_M = 77;
+    constexpr int  RB_N = 78;
+    constexpr int  RB_O = 79;
+    constexpr int  RB_P = 80;
+    constexpr int  RB_Q = 81;
+    constexpr int  RB_R = 82;
+    constexpr int  RB_S = 83;
+    constexpr int  RB_T = 84;
+    constexpr int  RB_U = 85;
+    constexpr int  RB_V = 86;
+    constexpr int  RB_W = 87;
+    constexpr int  RB_X = 88;
+    constexpr int  RB_Y = 89;
+    constexpr int  RB_Z = 90;
 
 
-        RB_LEFT_BRACKET = 91,   // <[>
-        RB_BACKSLASH = 92,  // <\>
-        RB_RIGHT_BRACKET = 93,  // <]>
-        RB_GRAVE_ACCENT = 96,   // <`>
+    constexpr int RB_LEFT_BRACKET = 91;   // <[>
+    constexpr int RB_BACKSLASH = 92;  // <\>
+    constexpr int RB_RIGHT_BRACKET = 93;  // <]>
+    constexpr int RB_GRAVE_ACCENT = 96;   // <`>
 
 
-        RB_ESCAPE = 256,
-        RB_ENTER = 257,
-        RB_TAB = 258,
-        RB_BACKSPACE = 259,
-        RB_INSERT = 260,
-        RB_DELETE = 261,
-        RB_RIGHT = 262,
-        RB_LEFT = 263,
-        RB_DOWN = 264,
-        RB_UP = 265,
-        RB_HOME = 268,
-        RB_END = 269,
-        RB_CAPS_LOCK = 280,
-        RB_PRINT_SCREEN = 283,
-        RB_PAUSE = 284,
+    constexpr int  RB_ESCAPE = 256;
+    constexpr int  RB_ENTER = 257;
+    constexpr int  RB_TAB = 258;
+    constexpr int  RB_BACKSPACE = 259;
+    constexpr int  RB_INSERT = 260;
+    constexpr int  RB_DELETE = 261;
+    constexpr int  RB_RIGHT = 262;
+    constexpr int  RB_LEFT = 263;
+    constexpr int  RB_DOWN = 264;
+    constexpr int  RB_UP = 265;
+    constexpr int  RB_HOME = 268;
+    constexpr int  RB_END = 269;
+    constexpr int  RB_CAPS_LOCK = 280;
+    constexpr int  RB_PRINT_SCREEN = 283;
+    constexpr int  RB_PAUSE = 284;
 
 
-        RB_F1 = 290,
-        RB_F2 = 291,
-        RB_F3 = 292,
-        RB_F4 = 293,
-        RB_F5 = 294,
-        RB_F6 = 295,
-        RB_F7 = 296,
-        RB_F8 = 297,
-        RB_F9 = 298,
-        RB_F10 = 299,
-        RB_F11 = 300,
-        RB_F12 = 301,
-        RB_F13 = 302,
-        RB_F14 = 303,
-        RB_F15 = 304,
-        RB_F16 = 305,
-        RB_F17 = 306,
-        RB_F18 = 307,
-        RB_F19 = 308,
-        RB_F20 = 309,
-        RB_F21 = 310,
-        RB_F22 = 311,
-        RB_F23 = 312,
-        RB_F24 = 313,
-        RB_F25 = 314,
+    constexpr int RB_F1 = 290;
+    constexpr int RB_F2 = 291;
+    constexpr int RB_F3 = 292;
+    constexpr int RB_F4 = 293;
+    constexpr int RB_F5 = 294;
+    constexpr int RB_F6 = 295;
+    constexpr int RB_F7 = 296;
+    constexpr int RB_F8 = 297;
+    constexpr int RB_F9 = 298;
+    constexpr int RB_F10 = 299;
+    constexpr int RB_F11 = 300;
+    constexpr int RB_F12 = 301;
+    constexpr int RB_F13 = 302;
+    constexpr int RB_F14 = 303;
+    constexpr int RB_F15 = 304;
+    constexpr int RB_F16 = 305;
+    constexpr int RB_F17 = 306;
+    constexpr int RB_F18 = 307;
+    constexpr int RB_F19 = 308;
+    constexpr int RB_F20 = 309;
+    constexpr int RB_F21 = 310;
+    constexpr int RB_F22 = 311;
+    constexpr int RB_F23 = 312;
+    constexpr int RB_F24 = 313;
+    constexpr int RB_F25 = 314;
 
 
-        // keypad keys
-        RB_KP_0 = 320,
-        RB_KP_1 = 321,
-        RB_KP_2 = 322,
-        RB_KP_3 = 323,
-        RB_KP_4 = 324,
-        RB_KP_5 = 325,
-        RB_KP_6 = 326,
-        RB_KP_7 = 327,
-        RB_KP_8 = 328,
-        RB_KP_9 = 329,
-        RB_KP_DECIMAL = 330,
-        RB_KP_DIVIDE = 331,
-        RB_KP_MULTIPLY = 332,
-        RB_KP_SUBTRACT = 333,
-        RB_KP_ADD = 334,
-        RB_KP_ENTER = 335,
-        RB_KP_EQUAL = 336,
+    // keypad keys
+    constexpr int RB_KP_0 = 320;
+    constexpr int RB_KP_1 = 321;
+    constexpr int RB_KP_2 = 322;
+    constexpr int RB_KP_3 = 323;
+    constexpr int RB_KP_4 = 324;
+    constexpr int RB_KP_5 = 325;
+    constexpr int RB_KP_6 = 326;
+    constexpr int RB_KP_7 = 327;
+    constexpr int RB_KP_8 = 328;
+    constexpr int RB_KP_9 = 329;
+    constexpr int RB_KP_DECIMAL = 330;
+    constexpr int RB_KP_DIVIDE = 331;
+    constexpr int RB_KP_MULTIPLY = 332;
+    constexpr int RB_KP_SUBTRACT = 333;
+    constexpr int RB_KP_ADD = 334;
+    constexpr int RB_KP_ENTER = 335;
+    constexpr int RB_KP_EQUAL = 336;
 
 
-        RB_LEFT_SHIFT = 340,
-        RB_LEFT_CONTROL = 341,
-        RB_LEFT_ALT = 342,
-        RB_LEFT_SUPER = 343,
-        RB_RIGHT_SHIFT = 344,
-        RB_RIGHT_CONTROL = 345,
-        RB_RIGHT_ALT = 346,
-        RB_RIGHT_SUPER = 347,
-        RB_MENU = 348,
-        RB_LAST = RB_MENU,
-    };
+    constexpr int  RB_LEFT_SHIFT = 340;
+    constexpr int  RB_LEFT_CONTROL = 341;
+    constexpr int  RB_LEFT_ALT = 342;
+    constexpr int  RB_LEFT_SUPER = 343;
+    constexpr int  RB_RIGHT_SHIFT = 344;
+    constexpr int  RB_RIGHT_CONTROL = 345;
+    constexpr int  RB_RIGHT_ALT = 346;
+    constexpr int  RB_RIGHT_SUPER = 347;
+    constexpr int  RB_MENU = 348;
+    constexpr int  RB_LAST = RB_MENU;
 }

512799ffcad92b439ee476c93e8229d7a69603c0
qulop <qlopsimail@gmail.com>
2024-04-05 19:23:26 +0300

    (Definitions) Added macros for compiler detection; RUBY_FORCEINLINE macro added

--- ruby/include/utility/Definitions.hpp
+++ ruby/include/utility/Definitions.hpp
@@ -5,32 +5,40 @@
 #include <cassert>   
 #include <ctype.h>          
 #include <functional>    
-#include "TypeTraits.hpp"           
+#include "TypeTraits.hpp"      
 
 
-#ifdef RUBY_BUILD           
+#ifdef _MSC_VER
+    #define RUBY_MSVC_USED
+#elif defined(__clang__)
+    #define RUBY_CLANG_USED
+#elif defined(__GNUC__) || defined(__GNUG__)
+    #define RUBY_GCC_USED
+#endif
+
+
+#ifdef RUBY_ENGINE_BUILD           
     #define RUBY_API                    __declspec(dllexport)
 #else               
     #define RUBY_API                    __declspec(dllimport)
-#endif  // RUBY_BUILD           
-            
-#ifdef __has_cpp_attribute          
-    #define RUBY_NODISCARD              [[nodiscard]]
-    #define RUBY_ASSERT_VAR             [[maybe_unused]]
-    #define RUBY_FALLTHROUGH            [[fallthrough]]
-    #define RUBY_NORETURN               [[noreturn]]
-#else           
-    #define RUBY_NODISCARD          
-    #define RUBY_ASSERT_VAR 
-    #define RUBY_FALLTHROUGH
-    #define RUBY_NORETURN          
-#endif  // __has_cpp_attribute    
+#endif 
+
+#define RUBY_NODISCARD              [[nodiscard]]
+#define RUBY_MAYBE_UNUSED           [[maybe_unused]]
+#define RUBY_FALLTHROUGH            [[fallthrough]]
+#define RUBY_NORETURN               [[noreturn]]
 
 #ifdef _NDEBUG            
     #define LOG_LEVEL                   spdlog::level::info
 #else
     #define LOG_LEVEL                   spdlog::level::debug
-#endif  // _NDEBUG
+#endif
+
+#ifdef RUBY_MSVC_USED
+    #define RUBY_FORCEINLINE            __forceinline
+#else
+    #define RUBY_FORCEINLINE            __attribute__(always_inline)
+#endif
 
                             
 #define RUBY_ASSERT(expr, msg)          assert(expr && msg)

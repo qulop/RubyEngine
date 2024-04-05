d76a7e93e38c5513b2604ffc8eaf4b82c592556d
qulop <qlopsimail@gmail.com>
2024-04-05 22:49:00 +0300

    (EnumReflector) Continue work on EnumReflector

--- ruby/include/utility/EnumReflector.hpp
+++ ruby/include/utility/EnumReflector.hpp
@@ -4,88 +4,118 @@
 #include <utility/Definitions.hpp>
 
 
-// namespace Ruby 
-// {
-//     namespace Details::Enum
-//     {
-//         struct Enumeration
-//         {
-//             RubyHashMap<RubyString, i32> data;
-//             RubyString enumName;
-//         };
+namespace Ruby 
+{
+    class EnumReflector;
 
-//         bool isAllowedChar(char ch);
+    namespace Details::Enum
+    {
+        RUBY_FORCEINLINE bool isAllowedChar(char ch)
+        { return std::isalpha(ch) || std::isdigit(ch) || ch == '_'; }
 
-//         // In this function bool indicated presence of error. 
-//         // getValue() function has three end scenario:
-//         //      1) will be returned i32 (i.e. field value);
-//         //      2) parser will come to end of string. In this case function will retun false, as 
-//         //          no error sign.
-//         //      3) function std::stoi() raise an exception and function will return true (i.e. presence of error)
-//         RUBY_NODISCARD std::expected<i32, bool> getValue(cstr str, i32& pos);
-//         RUBY_NODISCARD std::optional<RubyString> getField(cstr str, i32& pos);
-//     }
+        // In this function bool indicated presence of error. 
+        // getValue() function has three end scenario:
+        //      1) will be returned i32 (i.e. field value);
+        //      2) parser will come to end of string. In this case function will retun false, as 
+        //          no error sign.
+        //      3) function std::stoi() raise an exception and function will return true (i.e. presence of error)
+        RUBY_NODISCARD std::expected<i32, bool> getValue(cstr& str);
+        RUBY_NODISCARD std::optional<RubyString> getField(cstr& str);
+    }
+
+    class EnumField
+    {
+        RubyString GetEnumName(void) const;
+        i32 GetValue(void) const;
+        i32 GetIndex(void) const;
+        EnumReflector& GetReflector(void);
+
+        EnumField& GoForward(void);
+        EnumField& operator++(void);
+
+        operator bool(void) const;
+        bool IsHasValue(void) const;
+
+        const EnumField& operator*(void) const;
+
+    private:
+        explicit EnumField(std::shared_ptr<EnumReflector>&& reflector=nullptr, i32 index=-1);
+
+    private:
+        friend class EnumReflector;
+
+        const std::shared_ptr<EnumReflector> m_reflector = nullptr;
+        i32 m_index = -1;
+    };
+        
+
+    class EnumReflector
+    {
+        using EnumType = std::vector<std::pair<RubyString, i32>>;
+
+    public:
+        EnumReflector(const i32* values, i32 valuesNumber, cstr enumName, cstr strValues);
+        EnumReflector(const EnumReflector& other) = default;
 
 
-//     class EnumReflector
-//     {
-//     public:
-//         template<typename EnumType>
-//         static EnumReflector& Create(EnumType e)
-//         { return reflectCreator(e); }
+        template<typename EnumType>
+        static EnumReflector& Create(EnumType e=EnumType{})
+        { return reflectCreator(e); }
 
-//         size_t Size(void) const;
+        size_t Size(void) const;
+    
+        RubyString GetName(void) const;
 
+        EnumField GetByKey(const RubyString& key) const;
+        EnumField GetByValue(i32 value) const;
 
+        EnumField At(i32 i) const;
 
+    private:
+        EnumReflector(EnumReflector&& other);
+        
+    private:
+        EnumType m_enum;
+        RubyString m_enumName;
+    };
+}
 
+#define RUBY_ENUM_DETAILS_namespace                                             \
+        extern "C" {}                                                           \
+        inline                                                                  
 
+#define RUBY_ENUM_DETAILS_class friend                      
 
-//     private:
-//         EnumReflector(const i32* values, i32 valuesNumber, cstr enumName, cstr strValues);
-//         EnumReflector(EnumReflector&& other);
-
-//     private:
-//         Details::Enum::Enumeration m_enum;
-//     };
-// }
-
-// #define RUBY_ENUM_DETAILS_namespace                                             \
-//         extern "C"  {}                                                          \
-//         inline   
-
-// #define RUBY_ENUM_DETAILS_class friend                      
-
-// #define RUBY_CREATE_ENUM(enumName, EnumType, IntType, ...)                      \
-//     enum enumName : IntType                                                     \
-//     {                                                                           \
-//         __VA_ARGS__                                                             \
-//     }                                                                           \
-//                                                                                 \
-//     RUBY_ENUM_DETAILS_##EnumType Ruby::EnumReflector& reflectCreator(enumName)  \
-//     {                                                                           \
-//         static const Ruby::EnumReflector reflector{ []() {                      \
-//             static IntType nextValue;                                           \
-//             nextValue = 0;                                                      \
-//                                                                                 \
-//             struct Value {                                                      \
-//                 Value(void) : val(nextValue) { nextValue += 1; }                \
-//                 Value(int v) : val(v) { nextValue = val + 1; }                  \
-//                 Value(const Value& other) : val(other) { nextValue = val + 1; } \                                    \
-//                                                                                 \
-//                 Value& operator=(const Value&) { return *this; }                \
-//                 Value& operator=(int) { return *this; }                         \
-//                 operator IntType() const { return val; }                        \
-//                 IntType val;                                                    \
-//             } __VA_ARGS__;                                                      \
-//                                                                                 \
-//             const IntType values[] = { __VA_ARGS__ };                           \                   
-//             size_t valSize = sizeof(values) / sizeof(IntType);                  \
-//             return Ruby::EnumReflector(values, valSize,                         \ 
-//                                     RUBY_MAKE_STRING(enumName),                 \ 
-//                                     RUBY_MAKE_STRING((__VA_ARGS__)));           \
-//             }()                                                                 \ 
-//         };                                                                      \
-//                                                                                 \
-//         return reflector;                                                       \
-//     }                                                                           
+#define RUBY_CREATE_ENUM(enumName, EnumType, IntType, ...)                      \
+    enum enumName : IntType                                                     \
+    { __VA_ARGS__ };                                                            \
+                                                                                \
+    RUBY_ENUM_DETAILS_##EnumType Ruby::EnumReflector& reflectCreator(enumName)  \
+    {                                                                           \
+        static Ruby::EnumReflector reflector{ []() {                            \
+            static IntType nextValue;                                           \
+            nextValue = 0;                                                      \
+                                                                                \
+            struct Value {                                                      \
+                Value(void) : val(nextValue) { nextValue += 1; }                \
+                Value(int v) : val(v) { nextValue = val + 1; }                  \
+                Value(const Value& other) : val(other) { nextValue = val + 1; } \
+                                                                                \
+                Value& operator=(const Value&) { return *this; }                \
+                Value& operator=(int) { return *this; }                         \
+                operator IntType() const { return val; }                        \
+                IntType val;                                                    \
+            } __VA_ARGS__;                                                      \
+                                                                                \
+            const IntType values[] = { __VA_ARGS__ };                           \
+            IntType valSize = sizeof(values) / sizeof(IntType);                 \
+                                                                                \
+            return Ruby::EnumReflector(values,                                  \
+                                       valSize,                                 \
+                                       RUBY_MAKE_STRING(enumName),              \
+                                       RUBY_MAKE_STRING((__VA_ARGS__)));        \
+            }()                                                                 \
+        };                                                                      \
+                                                                                \
+        return reflector;                                                       \
+    }                                                                           
--- ruby/src/utility/EnumReflector.cpp
+++ ruby/src/utility/EnumReflector.cpp
@@ -1,94 +1,158 @@
-// #include <utility/EnumReflector.hpp>
-// #include <utility/RubyUtility.hpp>
+#include <utility/EnumReflector.hpp>
+#include <utility/RubyUtility.hpp>
 
 
-// namespace Ruby
-// {
-//     namespace Details::Enum
-//     {
-//         bool isAllowedChar(char ch)
-//         { return std::isalpha(ch) || std::isdigit(ch) || ch == '_'; }
+namespace Ruby
+{
+    namespace Details::Enum
+    {
+        std::expected<i32, bool> getValue(cstr& str)
+        {
+            RubyString value;
+            for (; *str != ')'; str++)
+            {
+                char ch = *str;
+                RUBY_ASSERT_1(ch);
 
-//         std::expected<i32, bool> getValue(cstr& str)
-//         {
-//             RubyString value;
-//             for (; *str != ')'; str++)
-//             {
-//                 char ch = *str;
-//                 RUBY_ASSERT(ch);
+                if (ch == ' ' || ch == ',')
+                {
+                    if (ch == ' ')
+                        continue;
+                    try { return std::stoi(value); }
+                    catch(...) { return true; }
+                }
 
-//                 if (ch == ' ' || ch == ',')
-//                 {
-//                     if (ch == ' ')
-//                         continue;
-//                     try { return std::stoi(value); }
-//                     catch(...) { return true; }
-//                 }
+                if (isAllowedChar(ch))
+                   value.push_back(ch);
+            }
 
-//                 if (isAllowedChar(ch))
-//                    value.push_back(ch);
-//             }
+            return false;
+        }
 
-//             return false;
-//         }
+        std::optional<RubyString> getField(cstr& str)
+        {
+            RubyString field;
+            for (; *str != ')'; str++)
+            {
+                char ch = *str;
+                RUBY_ASSERT_1(ch);
 
-//         std::optional<RubyString> getField(cstr& str)
-//         {
-//             RubyString field;
-//             for (; *str != ')'; str++)
-//             {
-//                 char ch = *str;
-//                 RUBY_ASSERT(ch);
+                if (ch == ' ' || ch == ',')
+                    break;
 
-//                 if (ch == ' ' || ch == ',')
-//                     break;
+                if (!isAllowedChar(ch))
+                {
+                    RUBY_ERROR("Details::Enum::getField : symbol {} isn't allowed here.", ch);
+                    return std::nullopt;
+                }
 
-//                 if (!isAllowedChar(ch))
-//                 {
-//                     RUBY_ERROR("Details::Enum::getField : symbol {} isn't allowed here.", ch);
-//                     return std::nullopt;
-//                 }
+                field.push_back(ch);
+            }
 
-//                 field.push_back(ch);
-//             }
+            return field;
+        }   
+    }
 
-//             return field;
-//         }   
-//     }
+    RubyString EnumField::GetEnumName(void) const
+    { return m_reflector->GetName();; }
+
+    i32 EnumField::GetValue(void) const
+    { return m_reflector->At(m_index); }
+
+    i32 EnumField::GetIndex(void) const
+    { return m_index; }
+
+    EnumReflector& EnumField::GetReflector(void)
+    { return *m_reflector; }
+
+    EnumField& EnumField::GoForward(void)
+    { 
+        m_index = (m_index < m_reflector->Size()) ? ++m_index : -1;
+        return *this;
+    }
+
+    EnumField& EnumField::operator++(void)
+    { return GoForward(); }
+
+    bool EnumField::IsHasValue(void) const
+    { return m_index != -1; }
+
+    EnumField::operator bool(void) const
+    { return IsHasValue();  }
 
 
+    const EnumField& EnumField::operator*(void) const
+    { return *this; }
 
-//     EnumReflector::EnumReflector(const i32* values, i32 valuesNumber, cstr enumName, cstr strValues)
-//     {
-//         m_enum.enumName = enumName;
 
-//         RUBY_ASSERT(*strValues == '(', 
-//             "EnumReflector : first symbol of strValues must be '('. Edit your code to '(__VA_ARGS__)'.");
-//         ++strValues;
+    EnumField::EnumField(std::shared_ptr<EnumReflector>&& reflector, i32 index) :
+        m_reflector(std::move(reflector)),
+        m_index(index)
+    {}
 
-//         size_t nextValue = 0;
-//         while (*strValues != ')')
-//         {
-//             RUBY_ASSERT_1(*strValues);
-//             if (*strValues == ' ')
-//                 continue;
 
-//             auto&& fieldName = Details::Enum::getField(strValues);
-//             auto&& fieldValue = Details::Enum::getValue(strValues);
+    EnumReflector::EnumReflector(const i32* values, i32 valuesNumber, cstr enumName, cstr strValues)
+    {
+        m_enumName = enumName;
+
+        RUBY_ASSERT(*strValues == '(', 
+            "EnumReflector : first symbol of strValues must be '('. Edit your code to '(__VA_ARGS__)'.");
+        ++strValues;
+
+        i32 nextValue = 0;
+        while (*strValues != ')')
+        {
+            RUBY_ASSERT_1(*strValues);
+            if (*strValues == ' ')
+                continue;
+
+            auto&& fieldName = Details::Enum::getField(strValues);
+            auto&& fieldValue = Details::Enum::getValue(strValues);
             
-//             if (!fieldName || (!fieldValue && fieldValue.error()))
-//             {
-//                 RUBY_ERROR("EnumReflector : failed to parse specified enum.");
-//                 return;
-//             }
+            if (!fieldName || (!fieldValue && fieldValue.error()))
+            {
+                RUBY_ERROR("EnumReflector : failed to parse specified enum.");
+                return;
+            }
     
-//             m_enum.data[fieldName.value()] = (fieldValue.has_value()) ? fieldValue.value() : nextValue;
-//             nextValue = (fieldValue.has_value()) ? ++fieldValue.value() : ++nextValue;
+            i32 value = (fieldValue.has_value()) ? fieldValue.value() : nextValue;
+            
+            m_enum.emplace_back(
+                std::make_pair(fieldName.value(), value));
+            nextValue = (fieldValue.has_value()) ? ++fieldValue.value() : ++nextValue;
         
-//             RUBY_INFO("EnumName: {}", m_enum.enumName);
-//             for (auto&& [n, v] : m_enum.data) {
-//                 RUBY_INFO("Field name: {}, value: {}", n, v);
-//             }
-//         }
-//     }
-// }
+            RUBY_INFO("EnumName: {}", m_enumName);
+            for (auto&& [n, v] : m_enum) {
+                RUBY_INFO("Field name: {}, value: {}", n, v);
+            }
+        }
+    }
+
+    size_t EnumReflector::Size(void) const
+    { return m_enum.size(); }
+
+    EnumField EnumReflector::At(i32 i) const
+    {
+        return (i < m_enum.size()) ?  EnumField{ std::make_shared<EnumReflector>(*this), i } : 
+                                    EnumField{};
+    }
+
+    RubyString EnumReflector::GetName(void) const
+    { return m_enumName; }
+
+    EnumField EnumReflector::GetByKey(const RubyString& key) const
+    {
+        for (i32 i = 0; i < m_enum.size(); i++)
+            if (m_enum.at(i).first == key)
+                return At(i);
+        return EnumField{};
+    }
+
+    EnumField EnumReflector::GetByValue(i32 value) const
+    {
+        for (i32 i = 0; i < m_enum.size(); i++)
+            if (m_enum.at(i).second == value)
+                return At(i);
+        return EnumField{};
+    }
+}

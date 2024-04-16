a04a8ebde2912c522ce86755b9109c4200997523
qulop <qlopsimail@gmail.com>
2024-04-16 22:42:20 +0300

    (Core) Credits.hpp added

---
+++ ruby/include/core/Credits.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+
+namespace Ruby
+{
+    template<typename OStream>
+    void printRubyCredits(OStream stream)
+    {
+        stream << "RubyEngine DEV.V\n";
+        stream << "Directors by inspiration:\n";
+        stream << "   * Amy\n";
+        stream << "   * Clara\n";
+        stream << "   * Victoria\n";
+        stream << "---------";
+        stream << "Developed by Qulop";
+        stream << "---------";
+        stream << "Special thanks to the next libraries: FreeType, Glad, GLFW, GLM, Json(by Nlohmann), SpdLog, Stb, Soil.\n";
+        stream << "---------";
+    }
+}

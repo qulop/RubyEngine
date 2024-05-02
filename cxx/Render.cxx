cb6ee18e748a0c435a32a7a57aff9dc64a860042
qulop <qlopsimail@gmail.com>
2024-05-02 21:58:41 +0300

    (CXX) Added build target for render

---
+++ cxx/Render.cxx
@@ -0,0 +1,4 @@
+#include "../ruby/src/render/Buffers.cpp"
+#include "../ruby/src/render/VertexArray.cpp"
+#include "../ruby/src/render/gl/GlBuffers.cpp"
+#include "../ruby/src/render/gl/GlVertexArray.cpp"

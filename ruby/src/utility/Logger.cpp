a0261ced7a0027aff46ef6e8e16b871253400491
qulop <qlopsimail@gmail.com>
2024-08-09 17:03:27 +0300

    (Utility) Now RUBY_CRITICAL will kill application with an error box

--- ruby/src/utility/Logger.cpp
+++ ruby/src/utility/Logger.cpp
@@ -1,7 +1,18 @@
 #include <utility/Logger.hpp>
 
+#include <platform/Platform.hpp>
+
+
 namespace Ruby {
-    Logger::Ptr<LoggerTraits::VendorLogger> Logger::MakeLog() const {
+    namespace Details::Log2 {
+        void destroyApp(const RubyString& msg) {
+            Platform::errorBox(msg.c_str(), "Critical Error!");
+            std::abort();
+        }
+    }
+
+
+    Logger::Ptr<Details::Log2::VendorLogger> Logger::GetVendorLogger() const {
         RUBY_ASSERT(m_logger != nullptr, "Logger cannot be empty: You must first call Logger::Init() before making log!");
 
         return m_logger;
@@ -9,7 +20,7 @@ namespace Ruby {
 
 
     void Logger::Init(const RubyString& pathToLogFile, const RubyString& coreName) {
-        auto console = std::make_shared<LoggerTraits::ConsoleSink>(spdlog::color_mode::always);
+        auto console = std::make_shared<Details::Log2::ConsoleSink>(spdlog::color_mode::always);
         console->set_pattern("<%m-%d-%Y %H:%M:%S> %^[%l]: %v%$");
 
         
@@ -21,19 +32,19 @@ namespace Ruby {
 
 
         // it will create new log file every 01:00 am
-        auto daily = std::make_shared<LoggerTraits::DailySink>(pathToLogFile, 1, 0);
+        auto daily = std::make_shared<Details::Log2::DailySink>(pathToLogFile, 1, 0);
         daily->set_pattern("[%l] <%m-%d-%Y %H:%M:%S> - [thread: %t] [PID: %P]: %v");
 
 
         std::vector<spdlog::sink_ptr> sinks = { std::move(console), std::move(daily) };
 
-        m_logger = std::make_shared<LoggerTraits::VendorLogger>(coreName, sinks.begin(), sinks.end());
+        m_logger = std::make_shared<Details::Log2::VendorLogger>(coreName, sinks.begin(), sinks.end());
 
         m_logger->set_level(LOG_LEVEL);
         m_logger->flush_on(LOG_LEVEL);
         spdlog::register_logger(m_logger);
 
         #undef LOG_LEVEL
-    }    
+    }
 }
 
--- ruby/src/utility/Logger.hpp
+++ ruby/src/utility/Logger.hpp
@@ -16,38 +16,58 @@
 
 
 namespace Ruby {
-    namespace LoggerTraits {
+    namespace Details::Log2 {
         using VendorLogger      = spdlog::logger;
         using DailySink         = spdlog::sinks::daily_file_sink_mt;
         using ConsoleSink       = spdlog::sinks::stdout_color_sink_mt;
 
         const RubyString defaultPath = "logs/log-from.txt";
         const RubyString defaultName = "RubyCore";
+
+        void destroyApp(const RubyString& msg);
     }
 
 
     class RUBY_API Logger final : public Singleton<Logger> {
-        DEFINE_SINGLETON(Logger);
-    
+        RUBY_DEFINE_SINGLETON(Logger);
+
     public:
         template<typename Tx>
         using Ptr = std::shared_ptr<Tx>;
 
-        RUBY_NODISCARD Ptr<LoggerTraits::VendorLogger> MakeLog() const;
+        RUBY_NODISCARD Ptr<Details::Log2::VendorLogger> GetVendorLogger() const;
 
-        void Init(const RubyString& pathToLogFile=LoggerTraits::defaultPath, 
-                    const RubyString& coreName=LoggerTraits::defaultName);
+        void Init(const RubyString& pathToLogFile=Details::Log2::defaultPath,
+                    const RubyString& coreName=Details::Log2::defaultName);
 
         private:
-            Ptr<LoggerTraits::VendorLogger> m_logger = nullptr;
+            Ptr<Details::Log2::VendorLogger> m_logger = nullptr;
     };
+
+
+    namespace Details::Log2 {
+        template<typename... Args>
+        void critical(spdlog::format_string_t<Args...> format, Args&&... args) {
+            auto&& msg = fmt::format(format, std::forward<Args>(args)...);
+            Logger::GetInstance().GetVendorLogger()->critical(std::move(format), std::forward<Args>(args)...);
+
+            destroyApp(msg);
+        }
+
+        template<typename Tx>
+        void critical(const Tx& format) {
+            Logger::GetInstance().GetVendorLogger()->critical(format);
+
+            destroyApp(format);
+        }
+    }
 }
 
-#define RUBY_DEBUG(...)            Ruby::Logger::GetInstance().MakeLog()->debug(__VA_ARGS__)
-#define RUBY_INFO(...)             Ruby::Logger::GetInstance().MakeLog()->info(__VA_ARGS__)
-#define RUBY_WARNING(...)          Ruby::Logger::GetInstance().MakeLog()->warn(__VA_ARGS__)
-#define RUBY_ERROR(...)            Ruby::Logger::GetInstance().MakeLog()->error(__VA_ARGS__)
-#define RUBY_CRITICAL(...)         Ruby::Logger::GetInstance().MakeLog()->critical(__VA_ARGS__)
+#define RUBY_DEBUG(...)            Ruby::Logger::GetInstance().GetVendorLogger()->debug(__VA_ARGS__)
+#define RUBY_INFO(...)             Ruby::Logger::GetInstance().GetVendorLogger()->info(__VA_ARGS__)
+#define RUBY_WARNING(...)          Ruby::Logger::GetInstance().GetVendorLogger()->warn(__VA_ARGS__)
+#define RUBY_ERROR(...)            Ruby::Logger::GetInstance().GetVendorLogger()->error(__VA_ARGS__)
+#define RUBY_CRITICAL(...)         Ruby::Details::Log2::critical(__VA_ARGS__)
 
 #ifdef RUBY_MSVC_USED
     #pragma warning(pop)

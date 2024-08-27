c184ab0fb45c97812ce226ebecc2dacc689dbf38
qulop <qlopsimail@gmail.com>
2024-08-27 20:26:43 +0300

    (Utility) Added more correct error handling(in case of unallocated memory) in Assert.cpp

--- ruby/src/utility/Assert.cpp
+++ ruby/src/utility/Assert.cpp
@@ -8,24 +8,27 @@ namespace Ruby::Details::Utility {
      RUBY_FORCEINLINE const char* _getFormattedString(bool prefix, const char* expr,
                                                    const char* file, u32 line, const char* func, const char* msg) {
         size_t msgSize = snprintf(nullptr, 0, "%sExpression: %s, File: %s, Line: %d, Function: %s --- %s.",
-                (prefix) ? "Assertion Failed: " : "",
-                expr,
-                file,
-                line,
-                (func) ? func : "Unknown",
-                (msg) ? msg : "");
+                    (prefix) ? "Assertion Failed: " : "",
+                    expr,
+                    file,
+                    line,
+                    (func) ? func : "Unknown",
+                    (msg) ? msg : "") + 1;  // + 1 for '\0'
 
-        char* buffer = static_cast<char*>(std::malloc(msgSize));
+        char* buffer = new char(msgSize);
         if (!buffer)
-         return "Failed to allocate memory for assertion message: Unknown error was occurred";
+            return nullptr;
 
-        snprintf(buffer, msgSize, "%sExpression: %s, File: %s, Line: %d, Function: %s --- %s.",
-            (prefix) ? "Assertion Failed: " : "",
-            expr,
-            file,
-            line,
-            (func) ? func : "",
-            (msg) ? msg : "");
+        size_t written = snprintf(buffer, msgSize, "%sExpression: %s, File: %s, Line: %d, Function: %s --- %s.",
+                    (prefix) ? "Assertion Failed: " : "",
+                    expr,
+                    file,
+                    line,
+                    (func) ? func : "",
+                    (msg) ? msg : "");
+
+        if (written != msgSize)
+            return nullptr;
 
         return buffer;
      }
@@ -34,12 +37,16 @@ namespace Ruby::Details::Utility {
      RUBY_FORCEINLINE bool _assertBase(const char* expr, bool prefix, const char* file, u32 line,
                                        const char* func, const char* msg) {
         const char* res = _getFormattedString(prefix, expr, file, line, func, msg);
-        fprintf_s(stderr, res);
+        if (res) {
+            fprintf_s(stderr, res);
+            delete res;
+        }
+        else {
+            fprintf_s(stderr, "Failed to allocate memory for assertion message: Unknown error was occurred");
+        }
 
         #ifndef _RUBY_CONFIG_ASSERT_IGNORE_ABORT
             std::abort();
-        #else
-            std::free(buffer);
         #endif
 
         return false;

2a3a9d193e8166ad88a74b8099d198b28ed1f513
qulop <qlopsimail@gmail.com>
2024-08-28 20:20:53 +0300

    (Core) Added StopWatch class in Timer.hpp

--- ruby/src/core/Timer.hpp
+++ ruby/src/core/Timer.hpp
@@ -13,4 +13,37 @@ namespace Ruby::Time {
 
         return now.time_since_epoch().count();
     }
+
+
+    template<typename ClockType = time::steady_clock, 
+            typename TimePoint = ClockType::time_point>
+    class StopWatch {
+        using Duration = ClockType::duration;
+
+    public:
+        StopWatch() : 
+            m_begin(Now())
+        {}
+
+        static TimePoint Now() {
+            return TimePoint::now();
+        }
+
+        Duration Elapsed() {
+            return Now() - m_begin;
+        }
+
+        Duration FetchRestart() {
+            auto tmp = m_begin;
+            Restart();
+            return tmp;
+        }
+
+        void Restart() {
+            m_begin = Now();
+        }
+
+    private:
+        TimePoint m_begin;
+    };
 }

c83f81e711d13988ccc944d5168d5d9c0411df5f
qulop <qlopsimail@gmail.com>
2024-08-31 22:44:55 +0300

    (Tests) Added macro for tests entry point

---
+++ ruby/tests/Main.cpp
@@ -0,0 +1,5 @@
+#include "cases/TestCases.hpp"
+#include "framework/RubyTestsFramework.hpp"
+
+
+RUBY_RUN_ALL_TESTS()
--- ruby/tests/framework/RubyTestsFramework.hpp
+++ ruby/tests/framework/RubyTestsFramework.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "core/Definitions.hpp"
 #include "core/ITest.hpp"
 #include "core/TestsRuntime.hpp"
 #include "core/Asserts.hpp"
@@ -10,17 +9,17 @@
 #include "utility/Exceptions.hpp"
 
 
-#define RUBY_TESTS_SUITE                         namespace Ruby::UnitTests
-#define RUBY_ADD_TEST(label, test)               Ruby::Testify::TestsRuntime::GetInstance().AddTest(label, test)
+#define RUBY_TESTS_SUITE                                namespace Ruby::UnitTests
+#define RUBY_REGISTER_TEST(label, test)                 Ruby::Testify::TestsRuntime::GetInstance().Register(label, test)
 
 
 #define RUBY_DEFINE_TEST(label, test)                           \
     void _##test##Test();                                       \
                                                                 \
-    class test##Test : public Ruby::Testify::ITest {     \
+    class test##Test : public Ruby::Testify::ITest {            \
     public:                                                     \
         test##Test() {                                          \
-            RUBY_ADD_TEST(label, this);                         \
+            RUBY_REGISTER_TEST(label, this);                    \
         }                                                       \
                                                                 \
         RubyString GetName() const override {                   \
@@ -36,5 +35,9 @@
     void _##test##Test()  
 
 
-#define RUBY_TEST_CLASS(name)                   class name : public ITest
-#define RUBY_TEST_METHOD(name)                  RUBY_NODISCARD void name() override
+#define RUBY_RUN_ALL_TESTS()                                        \
+    int main(int argc, char** argv) {                               \
+        Ruby::Testify::getTestsRuntime().RunAllTests(argc, argv);   \
+                                                                    \
+        return 0;                                                   \
+    }

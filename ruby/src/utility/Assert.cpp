73bc5a13394d5142fe5bf268fc73fbffcce93f82
qulop <qlopsimail@gmail.com>
2024-09-01 23:58:51 +0300

    (Utility) Assert was updated: added ability to format messages

--- ruby/src/utility/Assert.cpp
+++ ruby/src/utility/Assert.cpp
@@ -5,59 +5,12 @@
 
 
 namespace Ruby::Details::Assert {
-    struct _AssertStruct {  // NOLINT
-        const char* msg = "Failed to allocate memory for assertion message: Unknown error was occurred";
-        bool isAllocated = false;   // was memory allocated for msg
-    };
+    RubyString _getAssertionString(const char* expr, const RubyString& msg, std::source_location& loc) {
+        auto res = std::format("Assertion failed:\n\t-- Expression: {}\n\t-- File: {}\n\t-- Line: {}\n",
+                               expr, loc.file_name(), loc.line());
 
-    _AssertStruct _getFormattedString(bool prefix, const char* expr,
-                                                   const char* file, u32 line, const char* func, const char* msg) {
-        size_t msgSize = snprintf(nullptr, 0, "%sExpression: %s, File: %s, Line: %d, Function: %s --- %s.\n",
-                    (prefix) ? "Assertion Failed: " : "", expr,
-                    file, line,
-                    (func) ? func : "Unknown", (msg) ? msg : "") + 1;  // + 1 for '\0'
-
-        // TODO: Replace to Ruby::Platform::virtualAlloc()
-        char* buffer = new(std::nothrow) char[msgSize];
-        if (!buffer)
-            return {};
-
-        size_t written = snprintf(buffer, msgSize, "%sExpression: %s, File: %s, Line: %d, Function: %s --- %s.\n",
-                    (prefix) ? "Assertion Failed: " : "", expr,
-                    file, line,
-                    (func) ? func : "", (msg) ? msg : "") + 1;
-
-        if (written != msgSize)
-            return {};
-
-        return { .msg=buffer, .isAllocated=true };
+        if(!msg.empty())
+            res += std::format("\t-- Message: {}\n", msg);
+        return res;
      }
-
-
-     bool _assertBase(const char* expr, bool prefix, const char* file, u32 line, const char* func, const char* msg) {
-        auto res = _getFormattedString(prefix, expr, file, line, func, msg);
-
-
-        // idk how disable this shit...
-        // fprintf_s(stderr, res.msg);
-
-        if (res.isAllocated)
-            delete res.msg; // TODO: Replace to Ruby::Platform::virtualFree()
-
-        // same...
-        // std::abort();
-
-        return false;
-     }
-
-
-    bool _rubyAssert(const char* expr, bool assertPrefix,
-                     const char* file, u32 line, const char* func, const char* msg) {
-        return _assertBase(expr, assertPrefix, file, line, func, msg);
-    }
-
-
-    bool _rubyAssert(const char* expr, bool assertPrefix, const char* msg, std::source_location&& loc) {
-        return _assertBase(expr, assertPrefix, loc.file_name(), loc.line(), loc.file_name(), msg);
-    }
 }
--- ruby/src/utility/Assert.hpp
+++ ruby/src/utility/Assert.hpp
@@ -5,12 +5,6 @@
         1) RUBY_ASSERT_WITH_EXPR_AND_MSG_IMPL() - which allow to set assert expression and clarifying message(the
             message will be shown if assert fails);
         2) RUBY_ASSERT_WITH_SINGLE_EXPR_IMPL() - allows to set only assert expression.
-
-    Also, you can change assert behavior using these two macro:
-        1) _RUBY_CONFIG_ASSERT_IGNORE_ABORT - if assert fails, std::abort() will not be called - the program will
-            continue execution;
-        2) _RUBY_CONFIG_ASSERT_IGNORE_OUTPUT - ignore (full)assertion output. If _RUBY_CONFIG_ASSERT_IGNORE_ABORT also
-            set, then an error will be completely ignored.
 */
 
 #pragma once
@@ -20,38 +14,45 @@
 #include <types/Singleton.hpp>
 
 
-#if defined(RUBY_MSVC_USED)
-    #define __RUBY_FUNC__           __FUNCTION__
-#elif defined(RUBY_GCC_USED) || defined(RUBY_CLANG_USED)
-    #define __RUBY_FUNC__           __PRETTY_FUNCTION__
-#endif
-
-
 namespace Ruby::Details::Assert {
-    bool _rubyAssert(const char* expr, bool assertPrefix,
-                     const char* file, u32 line, const char* func, const char* msg=nullptr);
+    RubyString _getAssertionString(const char* expr, const RubyString& msg, std::source_location& loc);
 
-    bool _rubyAssert(const char* expr,
-                     bool assertPrefix=true,
-                     const char* msg=nullptr,
-                     std::source_location&& loc = std::source_location::current());
+
+    template<typename... Args>
+    bool _rubyAssert(const char* expr, std::format_string<Args...> fmt="",
+                     std::source_location loc = std::source_location::current(), Args&&... args) {
+        RubyString msg = std::format(fmt, std::forward<Args>(args)...);
+        auto res = _getAssertionString(expr, msg, loc);
+
+        fprintf_s(stderr, res.c_str());
+
+         std::abort();
+
+        return false;
+    }
+
+
+    template<typename... Args>
+    void _rubyWreck(std::format_string<Args...> fmt, Args&&... args) {
+        RubyString msg = std::format(fmt, std::forward<Args>(args)...);
+
+        fprintf_s(stderr, msg.c_str());
+        std::abort();
+    }
 }
 
 
 #ifndef _NDEBUG
-    #define RUBY_ASSERT_WITH_EXPR_AND_MSG_IMPL(expr, msg, prefix)    \
-        (static_cast<bool>(expr) || (Ruby::Details::Assert::_rubyAssert(#expr, prefix, msg)))
+    #define RUBY_ASSERT(expr, fmt, ...)     \
+        (static_cast<bool>(expr) || (Ruby::Details::Assert::_rubyAssert(#expr, fmt, std::source_location::current(), __VA_ARGS__)))
 
-    #define RUBY_ASSERT_WITH_SINGLE_EXPR_IMPL(expr, prefix) \
-        (static_cast<bool>(expr) || (Ruby::Details::Assert::_rubyAssert(#expr, prefix)))
+    #define RUBY_ASSERT_BASIC(expr)         \
+        (static_cast<bool>(expr) || (Ruby::Details::Assert::_rubyAssert(#expr)))
 #else
-    #define RUBY_ASSERT_WITH_MSG_IMPL(expr, msg)
-    #define RUBY_ASSERT_SINGLE_EXPR_MACRO_IMPL(expr)
+    #define RUBY_ASSERT(expr, msg)
+    #define RUBY_ASSERT_BASIC(expr)
 #endif
 
 
-#define RUBY_ASSERT(expr, msg)          RUBY_ASSERT_WITH_EXPR_AND_MSG_IMPL(expr, msg, true)
-#define RUBY_ASSERT_BASIC(expr)         RUBY_ASSERT_WITH_SINGLE_EXPR_IMPL(expr, true)
-
-#define RUBY_WRECK(msg)                 RUBY_ASSERT_WITH_EXPR_AND_MSG_IMPL(false, msg, false)
+#define RUBY_WRECK(msg, ...)            Ruby::Details::Assert::_rubyWreck(msg, __VA_ARGS__)
 #define RUBY_NOT_IMPLEMENTED            RUBY_WRECK("An unimplemented method(function) was called")

f20b9817d704f5365015a87f3c96c2da0162034c
qulop <qlopsimail@gmail.com>
2024-09-01 22:46:31 +0300

    (Memory) Updated interface for allocators. Added AllocatedBlock class and default size for allocators

--- ruby/src/memory/IAllocatorTraits.hpp
+++ ruby/src/memory/IAllocatorTraits.hpp
@@ -4,15 +4,34 @@
 
 
 namespace Ruby::Memory {
+    namespace Details {
+        constexpr size_t ALLOC_DEFAULT_POOL_SIZE = 1024 * 64; // 64Kb
+    };
+
+
+    struct AllocatedBlock {
+        const size_t size = 0;
+        void* ptr = nullptr;
+
+        AllocatedBlock() = default;
+        AllocatedBlock(size_t s, void* p) :
+            size(s),
+            ptr(p)
+        {}
+    };
+
+
     interface IAllocatorTraits {
-        virtual void* Allocate(size_t n) = 0;
+        virtual AllocatedBlock Allocate(size_t n) = 0;
+
         void* allocate(size_t n) {
-            return Allocate(n);
+            return Allocate(n).ptr;
         }
 
-        virtual void Deallocate(void* ptr, size_t n) = 0;
+        virtual void Deallocate(AllocatedBlock block) = 0;
+
         void deallocate(void* ptr, size_t n) {
-            Deallocate(ptr, n);
+            Deallocate(AllocatedBlock{ n, ptr });
         }
     };
 }

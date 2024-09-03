c1414699e5266da5ad2667fd64ecad671377e839
qulop <qlopsimail@gmail.com>
2024-09-03 10:34:15 +0300

    (Memory) StackAllocator was implemented

---
+++ ruby/src/memory/StackAllocator.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "IAllocatorTraits.hpp"
+
+#include <types/TypeTraits.hpp>
+#include <types/Logger.hpp>
+#include <utility/Assert.hpp>
+
+
+namespace Ruby::Memory {
+    template<size_t size = Details::ALLOC_DEFAULT_POOL_SIZE>
+    class StackAllocator : public IAllocatorTraits {
+    public:
+        StackAllocator() {  // NOLINT
+            std::fill(std::begin(m_memory), std::end(m_memory), 0);
+        }
+
+        AllocatedBlock Allocate(size_t s) override {
+            if ((m_current + s) > (m_memory + size)) {
+                RUBY_ERROR("StackAllocator::Allocate() : StackAllocator does not have enough memory to allocate {} byte(with StackAllocator's size being {})",
+                           s, size);
+                return AllocatedBlock{};
+            }
+
+            void* ret = m_current;
+            m_current += s;
+
+            return AllocatedBlock{ s, ret };
+        }
+
+        void Deallocate(AllocatedBlock block) override {
+            RUBY_ASSERT(Owns(block),
+                    "Transmitted memory block({}) doesn't belong to this allocator(with borders from {} to {}",
+                        block.ptr, (void*)m_memory, (void*)(m_memory + size));
+            RUBY_ASSERT((block.ptr == m_current - block.size),
+                    "In StackAllocator you can deallocate only last allocated memory");
+
+            m_current -= block.size;
+        }
+
+        RUBY_NODISCARD size_t Size() const {
+            return size;
+        }
+
+        bool Owns(AllocatedBlock block) {
+            auto* ptr = (byte*)block.ptr;
+
+            return (ptr >= m_memory) && (ptr <= m_memory + size);
+        }
+
+    private:
+        alignas(std::max_align_t) byte m_memory[size];
+        byte* m_current = m_memory;
+    };
+}

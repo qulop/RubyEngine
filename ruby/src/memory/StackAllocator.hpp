732e3a9be1ac724590382ebe3601ef65452e5d45
qulop <qlopsimail@gmail.com>
2024-10-12 18:09:52 +0300

    (Memory) Added constraint for StackAllocator

--- ruby/src/memory/StackAllocator.hpp
+++ ruby/src/memory/StackAllocator.hpp
@@ -9,13 +9,14 @@
 
 namespace Ruby::Memory {
     template<size_t size = Details::ALLOC_DEFAULT_POOL_SIZE>
+        requires (size <= Details::STACK_ALLOC_MAX)
     class StackAllocator : public IAllocatorTraits {
     public:
         StackAllocator() {  // NOLINT
             std::fill(std::begin(m_memory), std::end(m_memory), 0);
         }
 
-        AllocatedBlock Allocate(size_t s) override {
+        RUBY_NODISCARD AllocatedBlock Allocate(size_t s) override {
             if ((m_current + s) > (m_memory + size)) {
                 RUBY_ERROR("StackAllocator::Allocate() : StackAllocator does not have enough memory to allocate {} byte(with StackAllocator's size being {})",
                            s, size);
@@ -29,7 +30,7 @@ namespace Ruby::Memory {
         }
 
         void Deallocate(AllocatedBlock block) override {
-            RUBY_ASSERT(Owns(block),
+            RUBY_ASSERT(IsOwns(block),
                     "Transmitted memory block({}) doesn't belong to this allocator(with borders from {} to {}",
                         block.ptr, (void*)m_memory, (void*)(m_memory + size));
             RUBY_ASSERT((block.ptr == m_current - block.size),
@@ -38,12 +39,12 @@ namespace Ruby::Memory {
             m_current -= block.size;
         }
 
-        RUBY_NODISCARD size_t Size() const {
+        RUBY_NODISCARD size_t GetSize() const {
             return size;
         }
 
-        bool Owns(AllocatedBlock block) {
-            auto* ptr = (byte*)block.ptr;
+        bool IsOwns(AllocatedBlock block) {
+            auto* ptr = block.GetPtr<byte>();
 
             return (ptr >= m_memory) && (ptr <= m_memory + size);
         }

c396be5eae3319c32ff8b21330f596896c5cb0d1
qulop <qlopsimail@gmail.com>
2025-08-12 02:19:12 +0300

    feat(utility): Implemented `Cast` class
    Added `Cast` class, which should from now be using instead of `static_cast()` by calling `Cast::To<>()`
    Besides, this class contains all popular casts(can be redone in the future)

--- engine/utility/Cast.hpp
+++ engine/utility/Cast.hpp
@@ -11,68 +11,75 @@
 
 
 namespace Ruby {
-    template<Concepts::Number Tx>
-    Opt<Tx> fromChars(std::string_view str, std::chars_format fmt = std::chars_format::general) {
-        using ValueType = std::decay_t<Tx>;
-
-        auto val = static_cast<ValueType>(0);
-        auto res = std::from_chars_result{ nullptr, std::errc::invalid_argument };
-
-        // `std::from_chars()` doesn't recognize plus sign as the first character
-        const auto* begin = (str.front() == '+') ? (str.data() + 1) : str.data();
-        const auto* end = (str.data() + str.size());
-
-        if constexpr (std::is_integral_v<ValueType>) {
-            res = std::from_chars(begin, end, val);
-        }
-        else {
-            res = std::from_chars(begin, end, val, fmt);
+    class Cast {
+    public:
+        template<typename Tx>
+        RUBY_NODISCARD RUBY_FORCEINLINE static Tx* IsInstanceOf(auto* ptr) {
+            return dynamic_cast<Tx*>(ptr);
         }
 
-        if (res.ec != std::errc{} || res.ptr == nullptr) {
-            return nullopt;
+        template<typename TargetType, typename SourceType>
+        RUBY_NODISCARD static constexpr TargetType To(const SourceType& cur) {
+            return static_cast<TargetType>(cur);
         }
 
-        return val;
-    }
+    public:
+        template<Concepts::Number Tx>
+        RUBY_NODISCARD static Opt<Tx> FromChars(std::string_view str, std::chars_format fmt = std::chars_format::general) {
+            using ValueType = std::decay_t<Tx>;
 
+            auto val = static_cast<ValueType>(0);
+            auto res = std::from_chars_result{ nullptr, std::errc::invalid_argument };
 
-    inline Opt<bool> strToBool(std::string_view str) {
-        String lowerStr{ str };
-        std::ranges::transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), tolower);
+            // `std::from_chars()` doesn't recognize plus sign as the first character
+            const auto* begin = (str.front() == '+') ? (str.data() + 1) : str.data();
+            const auto* end = (str.data() + str.size());
 
-        return lowerStr == "true";
-    }
+            if constexpr (std::is_integral_v<ValueType>) {
+                res = std::from_chars(begin, end, val);
+            }
+            else {
+                res = std::from_chars(begin, end, val, fmt);
+            }
 
-    RUBY_FORCEINLINE Opt<f32> strToFloat(std::string_view str, std::chars_format fmt = std::chars_format::general) {
-        return fromChars<f32>(str, fmt);
-    }
+            if (res.ec != std::errc{} || res.ptr == nullptr) {
+                return nullopt;
+            }
 
-    RUBY_FORCEINLINE Opt<f64> strToDouble(std::string_view str, std::chars_format fmt = std::chars_format::general) {
-        return fromChars<f64>(str, fmt);
-    }
+            return val;
+        }
 
-    template<std::integral IntegralType = i32>
-    RUBY_FORCEINLINE Opt<IntegralType> strToInt(std::string_view str) {
-        return fromChars<IntegralType>(str);
-    }
+        RUBY_NODISCARD static inline Opt<bool> StringToBool(std::string_view str) {
+            String lowerStr{ str };
+            std::ranges::transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), tolower);
 
-    template<std::integral EnumDerivedType = i32>
-    Opt<String> enumValueToString(Concepts::Enum auto en) {
-        using EnumType = std::decay_t<decltype(en)>;
+            return lowerStr == "true";
+        }
 
-        return EnumReflector::Create<EnumType>()
+        RUBY_NODISCARD RUBY_FORCEINLINE static Opt<f32> StringToFloat(std::string_view str, std::chars_format fmt = std::chars_format::general) {
+            return FromChars<f32>(str, fmt);
+        }
+
+        RUBY_NODISCARD RUBY_FORCEINLINE static Opt<f64> StringToDouble(std::string_view str, std::chars_format fmt = std::chars_format::general) {
+            return FromChars<f64>(str, fmt);
+        }
+
+        template<Concepts::Integral IntegralType = i32>
+        RUBY_NODISCARD RUBY_FORCEINLINE static Opt<IntegralType> StringToInt(std::string_view str) {
+            return FromChars<IntegralType>(str);
+        }
+
+        RUBY_NODISCARD RUBY_FORCEINLINE static Opt<String> IntToString(Concepts::Integral auto val) {
+            return std::to_string(val);
+        }
+
+        template<std::integral EnumDerivedType = i32>
+        RUBY_NODISCARD static Opt<String> EnumToString(Concepts::Enum auto en) {
+            using EnumType = std::decay_t<decltype(en)>;
+
+            return EnumReflector::Create<EnumType>()
                 .GetByValue(static_cast<EnumDerivedType>(en))
                 .GetFieldName();
-    }
-
-    template<typename Tx>
-    Tx* isInstanceOf(auto* ptr) {
-        return dynamic_cast<Tx*>(ptr);
-    }
-
-    template<typename Targ>
-    constexpr Targ cast(const auto& cur) {
-        return static_cast<Targ>(cur);
-    }
+        }
+    };
 }

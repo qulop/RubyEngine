f0f9ffe225ce4579212294895580e3edf908d9eb
qulop <qlopsimail@gmail.com>
2025-09-23 16:24:18 +0300

    refactor(renderer): The `AShader` abstract class has been refined:
    1) `UncompiledSourcesMap` alias has been moved to `PreprocessorGLSL`
    2) All virtual functions that was responsible for shaders compilation have been removed - now `AShaderCompiler` responsible for shaders compilation
    3) `PreprocessSource()` and `TryToGetCachedShader()` functions have been removed

--- engine/renderer/shaders/Shader.cpp
+++ engine/renderer/shaders/Shader.cpp
@@ -1,15 +1,8 @@
 #include "Shader.hpp"
 
-#include <renderer/shaders/PreprocessorGLSL.hpp>
-#include <renderer/shaders/ShaderCacheManager.hpp>
-
-#include <utility/Algorithm.hpp>
-
-#include <types/Logger.hpp>
-#include <types/Hash.hpp>
-
 
 namespace Ruby {
+    // TODO: Remove this function. Use `Cast<String>::ToShaderStage()` instead
     Opt<EShaderStage> AShader::StringToShaderStage(StringView stageName) {
         if      (stageName == "vertex")     return EShaderStage::VERTEX;
         else if (stageName == "geometry")   return EShaderStage::GEOMETRY;
@@ -17,62 +10,5 @@ namespace Ruby {
         else if (stageName == "compute")    return EShaderStage::COMPUTE;
 
         return nullopt;
-    }
-
-    Opt<AShader::UncompiledSourcesMap> AShader::PreprocessSource(const String& src) const {
-        PreprocessorGLSL preprocessor;
-
-        auto res = preprocessor.Preprocess(src);
-        if (res) {
-            return *res;
-        }
-        
-        auto kind = res.error().kind;
-        RUBY_ASSERT_BASIC(kind != GlslPreprocessError::NONE);
-
-        // TODO: Idk where am i should output error messages, so for now just skip it
-        switch (kind) {
-            case GlslPreprocessError::INCORRECT_STAGE_NAME:
-                break;
-            case GlslPreprocessError::END_OF_STAGE_MISSED:
-                break;
-            case GlslPreprocessError::TOKEN_ALREADY_DECLARED:
-                break;
-            case GlslPreprocessError::INCORRECT_PREPROCESSOR_PROPERTIES_COUNT:
-                break;
-        }
-
-        return nullopt;
-    }
-
-    Opt<ShaderCacheEntry> AShader::TryToGetCachedShader(const String& hashedShaderSource) const {
-         auto& shaderCacheManager = ShaderCacheManager::GetInstance();
-
-        Opt<ShaderCacheEntry> optCacheEntry = shaderCacheManager
-            .GetFromLocalCache(hashedShaderSource)
-            .or_else([&]() 
-            {
-                return shaderCacheManager
-                    .GetFromGlobalCache(hashedShaderSource)
-                    .transform(&ShaderCacheEntry::FromFileContent);
-            }
-        );
-
-        if (!optCacheEntry) {
-            return nullopt; 
-        }
-
-        ShaderCacheEntry& cacheEntry = optCacheEntry.value();
-        if (!shaderCacheManager.IsInLocalCache(hashedShaderSource)) {
-            if (!shaderCacheManager.AddToLocalCache(hashedShaderSource, cacheEntry)) RUBY_UNLIKELY {
-                RUBY_ERROR("ShaderGL::CompileShader() : Failed to add cache entry into the local cache");
-            }
-        }
-
-        RUBY_DEBUG("AShader::TryToGetCachedShader() : The shader \"{}\" successfully loaded from the cache",
-            hashedShaderSource
-        );
-
-        return cacheEntry;
     }
 }
--- engine/renderer/shaders/Shader.hpp
+++ engine/renderer/shaders/Shader.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <types/StdInc.hpp>
+#include <types/hash/Hash.hpp>
+
 #include <utility/Definitions.hpp>
 #include <utility/Enum.hpp>
 
@@ -16,9 +18,6 @@ namespace Ruby {
 
     class RUBY_ABSTRACT RUBY_API AShader {
     public:
-        using UncompiledSourcesMap = HashMap<EShaderStage, String>;
-
-    public:
         RUBY_NODISCARD static Opt<EShaderStage> StringToShaderStage(StringView stageName);
 
     public:
@@ -33,13 +32,7 @@ namespace Ruby {
         virtual void Bind() const = 0;
         virtual void Unbind() const = 0;
 
-        virtual void AddShader(EShaderStage stage, const String& src, bool overrideExistingStage = true) = 0;
-        virtual void AddShader(const String& src, bool overrideExistingStage = true) = 0;
-
         RUBY_NODISCARD virtual bool IsEmpty() const = 0;
-        RUBY_NODISCARD virtual bool IsReady() const = 0;
-
-        virtual void Compile() = 0;
 
         virtual void SetFloat(const char* uniName, f32 value) const = 0;
         virtual void SetFloat2(const char* uniName, const glm::vec2& vec) const = 0;
@@ -58,10 +51,5 @@ namespace Ruby {
         virtual void SetMat4(const char* uniName, const glm::mat4& mat) const = 0;
 
         virtual ~AShader() = default;
-
-    protected:
-        RUBY_NODISCARD Opt<UncompiledSourcesMap> PreprocessSource(const String& src) const;
-
-        RUBY_NODISCARD Opt<ShaderCacheEntry> TryToGetCachedShader(const String& hashedShaderSource) const;
     };
 }

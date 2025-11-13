6d25a9d739080e6c2a264feb553d1191d192ecfb
qulop <qlopsimail@gmail.com>
2025-11-14 01:56:35 +0300

    feat(types): added `EmptyString()` function to the `String.hpp` header

--- engine/types/String.hpp
+++ engine/types/String.hpp
@@ -7,12 +7,16 @@
 namespace Ruby::StringUtils {
     template<std::ranges::input_range Tx>
         requires Concepts::ConvertibleTo<std::ranges::range_value_t<Tx>, String>
-    String join(const Tx& rng, char sep = ' ') {
+    String Join(const Tx& rng, char sep = ' ') {
         return std::accumulate(
             std::next(rng.begin()), rng.end(), String{ rng.front() },
             [&](String&& acc, const auto& s) {
                 return acc + sep + s;
             }
         );
+    }
+
+    constexpr String EmptyString() {
+        return String{};
     }
 }

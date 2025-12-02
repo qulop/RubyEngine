e76218e717266d873dbbe8c5bb4ac1e21a5e17a0
qulop <qlopsimail@gmail.com>
2025-12-02 13:29:51 +0300

    feat(renderer): the `ARenderInstance` class now derived from the `AObject`. Added `ENABLE_DEBUG_CAPABILITIES` static constexpr variable

--- engine/renderer/pipeline/RenderInstance.hpp
+++ engine/renderer/pipeline/RenderInstance.hpp
@@ -2,17 +2,33 @@
 
 #include <utility/Definitions.hpp>
 
+#include <core/Object.hpp>
+
 #include <renderer/pipeline/GraphicAPI.hpp>
 
 
 namespace Kiwi {
-    KIWI_ABSTRACT class ARenderInstance {
+    KIWI_ABSTRACT class ARenderInstance : public AObject {
+        KIWI_CREATE_OBJECT(ARenderInstance)
+
+    public:
+    #ifdef KIWI_DEBUG_BUILD
+        static constexpr bool ENABLE_DEBUG_CAPABILITIES = true;
+    #else
+        static constexpr bool ENABLE_DEBUG_CAPABILITIES = false;
+    #endif
+
+    public:
+        using PFN_DebugCallback = std::function<void(StringView)>;
+
     public:
         KIWI_NODISCARD virtual bool Init() = 0;
+        KIWI_NODISCARD virtual bool SetupDebugLayerCallback(const PFN_DebugCallback& debugCallback) = 0;
+
         KIWI_NODISCARD virtual EGraphicAPI GetUsedAPI() const = 0;
 
         virtual void Shutdown() {}
 
-        virtual ~ARenderInstance() = default;
+        ~ARenderInstance() override = default;
     };
 }

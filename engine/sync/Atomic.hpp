a8b5ff2d6c6bcdb8eff10f5a1e779b62888ca35a
qulop <qlopsimail@gmail.com>
2026-01-13 14:21:44 +0300

    feat(sync): added aliases for `std::atomic`

--- engine/sync/Atomic.hpp
+++ engine/sync/Atomic.hpp
@@ -9,4 +9,19 @@ namespace Kiwi {
     constexpr auto MEM_ORDER_ACQUIRE = std::memory_order::acquire;
     constexpr auto MEM_ORDER_RELEASE = std::memory_order::release;
     constexpr auto MEM_ORDER_CONSUME = std::memory_order::consume;
+
+
+    template<typename T>
+    using Atomic = std::atomic<T>;
+
+    using AtomicBool = Atomic<bool>;
+
+    using AtomicU32 = Atomic<u32>;
+    using AtomicU64 = Atomic<u64>;
+
+    using AtomicI32 = Atomic<i32>;
+    using AtomicI64 = Atomic<i64>;
+
+    using AtomicF32 = Atomic<f32>;
+    using AtomicF64 = Atomic<f64>;
 }

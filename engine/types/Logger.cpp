c653777c7b11a75ad10a22fcce39f953e2b2afbe
qulop <qlopsimail@gmail.com>
2026-01-13 12:38:20 +0300

    feat(types): the old `Logger` class and old it's functions marked as deprecated

--- engine/types/Logger.cpp
+++ engine/types/Logger.cpp
@@ -4,27 +4,22 @@
 #include <utility/Assert.hpp>
 
 
+namespace Kiwi::Details::LegacyLoggerDetails {
+    const char* logsDirectory = "logs";
+    const char* defaultFileName = "log-from.log";
+    const char* defaultLoggerName = "LegacyLogger_KiwiEngine";
+}
+
 namespace Kiwi {
-    namespace Details::LoggerDetails {
-        void DestroyAppWithErrorBox(const String& msg) {
-            KIWI_ASSERT(false, "{} -- TODO: DestroyAppWithErrorBox", msg);
-        }
-
-        const char* logsDirectory = "logs";
-        const char* defaultFileName = "log-from.log";
-        const char* defaultLoggerName = "KiwiEngine";
-    }
-
-
-    void Logger::Init(Path loggerPath, const char* fileName, const char* coreName) {
-        loggerPath /= Details::LoggerDetails::logsDirectory;
+    void LegacyLogger::Init(Path loggerPath, const char* fileName, const char* coreName) {
+        loggerPath /= Details::LegacyLoggerDetails::logsDirectory;
         if (!std::filesystem::exists(loggerPath))
             std::filesystem::create_directory(loggerPath);
         loggerPath /= fileName;
 
         auto& instLogger = GetInstance().m_logger;
 
-        auto&& console = MakeShared<Details::LoggerDetails::ConsoleSink>(spdlog::color_mode::always);
+        auto&& console = MakeShared<Details::LegacyLoggerDetails::ConsoleSink>(spdlog::color_mode::always);
         console->set_pattern("<%m-%d-%Y %H:%M:%S> %^[%l]: %v%$");
 
 
@@ -35,12 +30,12 @@ namespace Kiwi {
         console->set_color(spdlog::level::critical, console->magenta);
 
         // it will create new log file every 01:00 am
-        auto&& daily = MakeShared<Details::LoggerDetails::DailySink>(std::move(loggerPath.string()), 1, 0);
+        auto&& daily = MakeShared<Details::LegacyLoggerDetails::DailySink>(std::move(loggerPath.string()), 1, 0);
         daily->set_pattern("[%l] <%m-%d-%Y %H:%M:%S> - [thread: %t] [PID: %P]: %v");
 
 
         Vector<spdlog::sink_ptr> sinks = { std::move(console), std::move(daily) };
-        instLogger = MakeShared<Details::LoggerDetails::VendorLogger>(coreName, sinks.begin(), sinks.end());
+        instLogger = MakeShared<Details::LegacyLoggerDetails::VendorLogger>(coreName, sinks.begin(), sinks.end());
 
         instLogger->set_level(KIWI_LOG_LEVEL);
         instLogger->flush_on(KIWI_LOG_LEVEL);
@@ -50,13 +45,13 @@ namespace Kiwi {
     }
 
 
-    SharedPtr<Details::LoggerDetails::VendorLogger> Logger::GetLogger() const {
-        KIWI_ASSERT(m_logger != nullptr, "Logger cannot be empty: You must first call Logger::Init() before making a log!");
+    SharedPtr<Details::LegacyLoggerDetails::VendorLogger> LegacyLogger::GetLogger() const {
+        KIWI_ASSERT(IsInitialized(), "Logger cannot be empty: You must first call Logger::Init() before making a log!");
 
         return m_logger;
     }    
 
-    bool Logger::IsInitialized() const {
+    bool LegacyLogger::IsInitialized() const {
         return m_logger != nullptr;
     }
 }
--- engine/types/Logger.hpp
+++ engine/types/Logger.hpp
@@ -16,66 +16,44 @@
 #include "StdInc.hpp"
 
 
+
+namespace Kiwi::Details::LegacyLoggerDetails {
+    using VendorLogger      = spdlog::logger;
+    using DailySink         = spdlog::sinks::daily_file_sink_mt;
+    using ConsoleSink       = spdlog::sinks::ansicolor_stdout_sink_mt;
+
+    extern const char* logsDirectory;
+    extern const char* defaultFileName;
+    extern const char* defaultLoggerName;
+}
+
 namespace Kiwi {
-    class KIWI_API Logger;  // Declaration for critical function (see bellow)
-
-
-    namespace Details::LoggerDetails {
-        using VendorLogger      = spdlog::logger;
-        using DailySink         = spdlog::sinks::daily_file_sink_mt;
-        using ConsoleSink       = spdlog::sinks::ansicolor_stdout_sink_mt;
-
-        extern const char* logsDirectory;
-        extern const char* defaultFileName;
-        extern const char* defaultLoggerName;
-    }
-
-
-    class KIWI_API Logger final : public Singleton<Logger> {
-        KIWI_DEFINE_SINGLETON(Logger);
+    class KIWI_API LegacyLogger final : public Singleton<LegacyLogger> {
+        KIWI_DEFINE_SINGLETON(LegacyLogger);
 
     public:
-        using LoggerPtr = SharedPtr<Details::LoggerDetails::VendorLogger>;
+        using LoggerPtr = SharedPtr<Details::LegacyLoggerDetails::VendorLogger>;
 
     public:
-        static void Init(Path loggerPath,
-                       const char* fileName = Details::LoggerDetails::defaultFileName,
-                       const char* coreName = Details::LoggerDetails::defaultLoggerName);
+        KIWI_DEPRECATED static void Init(Path loggerPath,
+                       const char* fileName = Details::LegacyLoggerDetails::defaultFileName,
+                       const char* coreName = Details::LegacyLoggerDetails::defaultLoggerName);
 
     public:
-        KIWI_NODISCARD LoggerPtr GetLogger() const;
-        KIWI_NODISCARD bool IsInitialized() const;
+        KIWI_DEPRECATED KIWI_NODISCARD LoggerPtr GetLogger() const;
+        KIWI_DEPRECATED KIWI_NODISCARD bool IsInitialized() const;
 
     private:
         LoggerPtr m_logger = nullptr;
     };
-
-
-    namespace Details::LoggerDetails {
-        void DestroyAppWithErrorBox(const String& msg);
-
-        template<typename... Args>
-        void Critical(spdlog::format_string_t<Args...> format, Args&&... args) {
-            auto&& msg = fmt::format(format, std::forward<Args>(args)...);
-            Logger::GetInstance().GetLogger()->critical(std::move(format), std::forward<Args>(args)...);
-
-            DestroyAppWithErrorBox(msg);
-        }
-
-        template<typename Tx>
-        void Critical(const Tx& format) {
-            Logger::GetInstance().GetLogger()->critical(format);
-
-            DestroyAppWithErrorBox(format);
-        }
-    }
 }
 
-#define KIWI_DEBUG(...)            Kiwi::Logger::GetInstance().GetLogger()->debug(__VA_ARGS__)
-#define KIWI_INFO(...)             Kiwi::Logger::GetInstance().GetLogger()->info(__VA_ARGS__)
-#define KIWI_WARNING(...)          Kiwi::Logger::GetInstance().GetLogger()->warn(__VA_ARGS__)
-#define KIWI_ERROR(...)            Kiwi::Logger::GetInstance().GetLogger()->error(__VA_ARGS__)
-#define KIWI_CRITICAL(...)         Kiwi::Details::LoggerDetails::Critical(__VA_ARGS__)
+
+#define KIWI_DEBUG(...)            Kiwi::LegacyLogger::GetInstance().GetLogger()->debug(__VA_ARGS__)
+#define KIWI_INFO(...)             Kiwi::LegacyLogger::GetInstance().GetLogger()->info(__VA_ARGS__)
+#define KIWI_WARNING(...)          Kiwi::LegacyLogger::GetInstance().GetLogger()->warn(__VA_ARGS__)
+#define KIWI_ERROR(...)            Kiwi::LegacyLogger::GetInstance().GetLogger()->error(__VA_ARGS__)
+#define KIWI_CRITICAL(...)         Kiwi::LegacyLogger::GetInstance().GetLogger()->critical(__VA_ARGS__)
 
 #ifdef KIWI_MSVC_USED
     #pragma warning(pop)

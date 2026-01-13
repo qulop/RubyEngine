2477c96632d9ce6b366411cd030a5ac5e37f5d72
qulop <qlopsimail@gmail.com>
2026-01-13 14:23:50 +0300

    feat(misc): added logic for creation window creation in `WindowSubsystem`

--- engine/misc/WindowSubsystem.cpp
+++ engine/misc/WindowSubsystem.cpp
@@ -2,12 +2,53 @@
 
 
 namespace Kiwi {
+    bool WindowSubsystem::Init() {
+        Super::Init();
+
+        return true;
+    }
+
+    bool WindowSubsystem::CreateMainWindow(StringView title) {
+        KIWI_ASSERT(!m_mainWindow, "Main window already created!");
+
+        Vector<Platform::DisplayInfo> displayInfos = Platform::EnumerateDisplays();
+        if (displayInfos.empty()) {
+            KIWI_CTX_LOG(ERROR, "Failed to enumerate system displays");
+            return false;
+        }
+
+        auto selectedDisplay = std::ranges::find_if(displayInfos, [](const auto& displayInfo) {
+            return Platform::IsDisplayCurrentlyActive(displayInfo);
+        });
+        if (selectedDisplay == displayInfos.end()) {
+            KIWI_CTX_LOG(WARNING, "Failed to find a preferred display to use. We will use the first one found earlier");
+            selectedDisplay = displayInfos.begin();
+        }
+
+        KIWI_CTX_LOG(INFO, "Selected display: {} - {} with resolution {}x{} and refresh rate {}Hz",
+            selectedDisplay->model,
+            selectedDisplay->name,
+            selectedDisplay->resolution.x,
+            selectedDisplay->resolution.y,
+            selectedDisplay->refreshRate
+        );
+
+        m_mainWindow = AWindow::CreateWindowImpl();
+        if (!m_mainWindow->Init(title, *selectedDisplay)) {
+            return false;
+        }
+
+        return true;
+    }
+
     SharedPtr<AWindow> WindowSubsystem::GetMainWindow() {
+        KIWI_ENSURE(m_mainWindow);
+
         return m_mainWindow;
     }
 
     Vector<const char*> WindowSubsystem::GetVulkanExtensionsForWindow(SharedPtr<AWindow> window) {
-        KIWI_ASSERT_BASIC(window);
+        KIWI_ENSURE(window);
 
         return window->GetVulkanExtensions();
     }
--- engine/misc/WindowSubsystem.hpp
+++ engine/misc/WindowSubsystem.hpp
@@ -10,9 +10,17 @@ namespace Kiwi {
         KIWI_CREATE_OBJECT(WindowSubsystem)
 
     public:
+        using Super = ASubsystem;
+
+    public:
+        bool Init() override;
+        KIWI_NODISCARD bool CreateMainWindow(StringView title);
+
         KIWI_NODISCARD SharedPtr<AWindow> GetMainWindow();
 
         KIWI_NODISCARD Vector<const char*> GetVulkanExtensionsForWindow(SharedPtr<AWindow> window);
+
+
 
     private:
         SharedPtr<AWindow> m_mainWindow;

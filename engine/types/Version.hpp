e85b959a7d2a9400294667dfeda822cd036b5c8d
qulop <qlopsimail@gmail.com>
2026-01-13 14:28:23 +0300

    feat(types): the `Version` structure made as `constexpr`

--- engine/types/Version.hpp
+++ engine/types/Version.hpp
@@ -3,29 +3,32 @@
 #include <types/TypeTraits.hpp>
 
 
-
 namespace Kiwi {
     struct Version {
     public:
         u32 versionFlags = 0;
 
     public:
-        Version() = default;
-        Version(u8 major, u8 minor, u8 patch) :
+        constexpr Version() = default;
+        constexpr Version(u8 major, u8 minor, u8 patch) :
             versionFlags(((u32)major << 22) | ((u32)minor << 12) | ((u32)patch))
         {}
 
     public:
-        KIWI_NODISCARD KIWI_FORCEINLINE u32 GetMajor() const {
+        KIWI_NODISCARD KIWI_FORCEINLINE constexpr u32 GetMajor() const {
             return versionFlags >> 22;
         }
 
-        KIWI_NODISCARD KIWI_FORCEINLINE u32 GetMinor() const {
+        KIWI_NODISCARD KIWI_FORCEINLINE constexpr u32 GetMinor() const {
             return (versionFlags >> 12) & 0x3FF;
         }
 
-        KIWI_NODISCARD KIWI_FORCEINLINE u32 GetPatch() const {
+        KIWI_NODISCARD KIWI_FORCEINLINE constexpr u32 GetPatch() const {
             return versionFlags & 0xFFF;
+        }
+
+        KIWI_NODISCARD constexpr u32 GetFlags() const {
+            return versionFlags;
         }
     };
 }

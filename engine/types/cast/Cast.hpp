13003c1418f2c32bcfd44d2ac389af687309666f
qulop <qlopsimail@gmail.com>
2026-01-18 17:21:06 +0300

    feat(types): fixed build error in the `CastTo()` function

--- engine/types/cast/Cast.hpp
+++ engine/types/cast/Cast.hpp
@@ -10,10 +10,25 @@ namespace Kiwi {
     using Cast = CastTraits<T, Args...>;
 
 
-    template<typename TTargetType, typename TSourceType>
-    KIWI_FORCEINLINE constexpr TTargetType CastTo(TSourceType&& s) {
-        static_assert(std::is_convertible_v<TTargetType, TSourceType>);
+    template<typename T>
+    KIWI_NODISCARD KIWI_FORCEINLINE T* IsInstanceOf(auto* ptr) {
+        return BasicCast::IsInstanceOf<T>(ptr);
+    }
 
+    template<typename TTargetType, typename TSourceType>
+    requires (!std::is_reference_v<TTargetType>) &&
+            std::is_convertible_v<std::decay_t<TSourceType>, TTargetType>
+    KIWI_NODISCARD KIWI_FORCEINLINE constexpr TTargetType CastTo(TSourceType&& s) {
         return BasicCast::To<TTargetType>(s);
+    }
+
+    template<typename TTargetType, typename TSourceType>
+    KIWI_NODISCARD KIWI_FORCEINLINE constexpr TTargetType UnsafeCast(const TSourceType& cur) {
+        return BasicCast::UnsafeCast<TTargetType, TSourceType>(cur);
+    }
+
+    template<typename TTargetType, typename TSourceType>
+    KIWI_NODISCARD KIWI_FORCEINLINE constexpr TTargetType HierarchyCast(const TSourceType& cur) {
+        return BasicCast::HierarchyCast<TTargetType, TSourceType>(cur);
     }
 }

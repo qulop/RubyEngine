bdd27c94fd12f79b75354e834b966393f04c0d47
qulop <qlopsimail@gmail.com>
2026-01-18 15:57:48 +0300

    refactor(audio): removed the deprecated logger

--- engine/audio/IAudioOutputStream.cpp
+++ engine/audio/IAudioOutputStream.cpp
@@ -3,8 +3,6 @@
 #include <platform/Platform.hpp>
 #include <win32/audio/WaveOutAudioOutputStream.hpp>
 
-#include <types/Logger.hpp>
-
 
 namespace Kiwi {
 	SharedPtr<IAudioOutputStream> IAudioOutputStream::Create(const AudioParams& params) {
@@ -12,9 +10,7 @@ namespace Kiwi {
             return MakeShared<Win32::WaveOutAudioOutputStream>(params);
         }
 	    else {
-            KIWI_CRITICAL(
-                "IAudioOutputStream::Create() : Failed to create instance of IAudioOutputStream -- your platform isn't supported for now");
-            return nullptr;
+            std::unreachable();
         }
     }
 }

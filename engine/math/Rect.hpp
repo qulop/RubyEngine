547d66423ecd935f89ce6a09b76e7fc4fe2aa7c8
qulop <qlopsimail@gmail.com>
2026-01-26 19:39:54 +0300

    feat(math): the `Rect<T>` type made as constexpr

--- engine/math/Rect.hpp
+++ engine/math/Rect.hpp
@@ -32,74 +32,74 @@ namespace Kiwi {
         VectorType bottomRight;
 
     public:
-        Rect() = default;
+        constexpr Rect() = default;
 
-        Rect(ValueType x0, ValueType y0, ValueType x1, ValueType y1) :
+        constexpr Rect(ValueType x0, ValueType y0, ValueType x1, ValueType y1) :
             topLeft(x0, y0),
             bottomRight(x1, y1)
         {}
 
-        Rect(const VectorType& topLeft, const VectorType& bottomRight) :
+        constexpr Rect(const VectorType& topLeft, const VectorType& bottomRight) :
             topLeft(topLeft),
             bottomRight(bottomRight)
         {}
 
-        Rect(const Rect& other) :
+        constexpr Rect(const Rect& other) :
             topLeft(other.topLeft),
             bottomRight(other.bottomRight)
         {}
 
-        Rect(Rect&& other) noexcept :
+        constexpr Rect(Rect&& other) noexcept :
             topLeft(std::move(other.topLeft)),
             bottomRight(std::move(other.bottomRight))
         {}
 
     public:
-        KIWI_NODISCARD bool IsInside(ValueType x, ValueType y) const {
+        KIWI_NODISCARD constexpr bool IsInside(ValueType x, ValueType y) const {
             return topLeft.x <= x && x <= bottomRight.x && topLeft.y <= y && y <= bottomRight.y;
         }
 
-        KIWI_NODISCARD VectorType Center() const {
+        KIWI_NODISCARD constexpr VectorType Center() const {
             return VectorType(bottomRight.x * 0.5f, bottomRight.y * 0.5f);
         }
 
-        KIWI_NODISCARD ValueType Width() const {
+        KIWI_NODISCARD constexpr ValueType Width() const {
             return topLeft.x - bottomRight.x;
         }
 
-        KIWI_NODISCARD ValueType Height() const {
+        KIWI_NODISCARD constexpr ValueType Height() const {
             return topLeft.y - bottomRight.y;
         }
 
-        KIWI_NODISCARD ValueType Area() const {
+        KIWI_NODISCARD constexpr ValueType Area() const {
             return Width() * Height();
         }
 
-        KIWI_NODISCARD VectorType MinimumCoordinate() const {
+        KIWI_NODISCARD constexpr VectorType MinimumCoordinate() const {
             return topLeft;
         }
 
-        KIWI_NODISCARD VectorType MaximumCoordinate() const {
+        KIWI_NODISCARD constexpr VectorType MaximumCoordinate() const {
             return bottomRight;
         }
 
 
-        KIWI_NODISCARD bool operator==(const Rect& other) const {
+        KIWI_NODISCARD constexpr bool operator==(const Rect& other) const {
             return topLeft == other.topLeft && bottomRight == other.bottomRight;
         }
 
-        KIWI_NODISCARD bool operator!=(const Rect& other) const {
+        KIWI_NODISCARD constexpr bool operator!=(const Rect& other) const {
             return !(*this == other);
         }
 
-        Rect& operator=(const Rect& other) {
+        constexpr Rect& operator=(const Rect& other) {
             topLeft = other.topLeft;
             bottomRight = other.bottomRight;
 
             return *this;
         }
 
-        Rect& operator=(Rect&& other) noexcept {
+        constexpr Rect& operator=(Rect&& other) noexcept {
             topLeft = std::move(other.topLeft);
             bottomRight = std::move(other.bottomRight);
 
@@ -110,7 +110,7 @@ namespace Kiwi {
 
     using FRect = Rect<f32>;
     using F64Rect = Rect<f64>;
-    using URect = Rect<u32>;
+    using U32Rect = Rect<u32>;
     using I32Rect = Rect<i32>;
     using I64Rect = Rect<i64>;
 }

f7ed05ccfd6cf4c9ffc815acc8e106a827d08f0c
busyonline <wwbusiness@atomicmail.io>
2026-02-05 17:33:03 +0300

    feat(utility): added signal system implementation
    closes #50

--- engine/utility/Signal.hpp
+++ engine/utility/Signal.hpp
@@ -1,2 +1,42 @@
 #pragma once
 
+#include <types/TypeTraits.hpp>
+
+namespace Kiwi {
+    template<typename... Args>
+    class Signal {
+    public:
+        using ListenerType = void(*)(Args...); 
+    
+    public:
+        Signal() = default;
+
+        Signal(ForwardList<ListenerType> listeners)
+        : listeners(listeners)
+        {}
+
+        Signal(const Signal& other) 
+        : listeners(other.listeners)
+        {}
+
+        Signal(Signal&& other)
+        : listeners(std::move(other.listeners))
+        {}
+        
+        ~Signal() = default;
+
+    public:
+        void AppendListener(ListenerType listener) {
+            this->listeners.push_front(listener);
+        }
+
+        void Fire(Args args...) {
+            for (auto& listener : this->listeners) {
+                listener(args...);
+            }
+        }
+        
+    private:
+        ForwardList<ListenerType> listeners;
+    };
+}
